d5791c89e633ff05976efd7962feb10b932b3ff2
xiaoxie110 <1198886546@qq.com>
2020-07-25 17:45:54 +0800

    'gitignore'

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TestController extends Controller
 {
-    public function requestTest(Request $request){
+    public function requestTest(Request $request, Response $response){
         dump($request->method());
         dump($request->url());
         dump($request->fullUrl());
