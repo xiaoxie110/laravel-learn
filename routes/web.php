b51d2f719e34da63728f316719231aae7ab73cbc
xiaoxie110 <1198886546@qq.com>
2020-07-26 12:52:55 +0800

    'request'

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,25 @@ Route::get('/', function () {
 // 测试
 Route::group(['prefix' => '/test', 'as' => 'test.'], function () {
     Route::get('/request', 'TestController@requestTest')->name('request');
+    Route::middleware('cache.headers:public;max_age=2628000;etag')->get('/test', function () {
+        return response('hello world')
+            ->withHeaders([
+                'Content-Type' => 'image/jpg',//设置响应头
+                'X-Header-One' => 'Header Value',
+            ])->cookie('name', 'value', 1);//添加cookie
+    });
+    Route::get('directTest', function () {
+        return redirect('/test/request');
+    });
+    Route::get('view', function(){
+        return response()
+            ->view('hello', ['data' => 'hello world'], 200)
+            ->header('Content-Type', 'text/html');
+    });
+    Route::get('json',function(){
+        return response()->json([
+            'a' => 'hello',
+            'b' => 'world'
+        ]);
+    });
 });
